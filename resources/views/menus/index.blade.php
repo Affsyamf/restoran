7ca2b998b0ad841aa2bd915c6f77a42adc9dae86
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-08-02 17:40:52 +0700

    perbaikan admin menu

--- resources/views/menus/index.blade.php
+++ resources/views/menus/index.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <div class="flex justify-between items-center mb-6">
         <h1 class="text-2xl font-bold">Daftar Menu</h1>
-        <a href="{{ route('menus.create') }}" class="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-500">
+        <a href="{{ route('admin.menus.create') }}" class="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-500">
             + Tambah Menu Baru
         </a>
     </div>
